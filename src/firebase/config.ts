bd244c167d8cd652e25dbd83ede72eecef326100
Jamshid Khakimjonov <jkhakimjonov8@gmail.com>
2026-03-27 20:27:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2916828899-aeb98",
+  "appId": "1:730273376700:web:20f3f8ac2b81ec4e2d23b9",
+  "apiKey": "AIzaSyBbBJX1pLlAtHGymyhdaKtQNjOARPO610c",
+  "authDomain": "studio-2916828899-aeb98.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "730273376700"
+};
