0df7b0a273114cbd4d19856f7a69d8bee1d5080b
Jamshid Khakimjonov <jkhakimjonov8@gmail.com>
2026-04-12 23:37:29 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev, hostname: '0.0.0.0', port: 6000 })
+const app = next({ dev, hostname: '0.0.0.0', port: 3000 })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
     handle(req, res, parsedUrl)
-  }).listen(6000, '0.0.0.0', () => {
-    console.log('> Ready on http://0.0.0.0:6000')
+  }).listen(3000, '0.0.0.0', () => {
+    console.log('> Ready on http://0.0.0.0:3000')
   })
 })
